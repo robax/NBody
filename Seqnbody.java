517888073784e05a630503891b823926c249f51f
Tanner Bernth <tannerbernth@MacBook-Pro-TB.local>
2017-04-03 19:13:51 -0700

    Changed sequential version to use the same files as the parallel version + object oriented

--- Seqnbody.java
+++ Seqnbody.java
@@ -1,5 +1,3 @@
-package sequential;
-
 /*---------------------------------------------------
  * Tanner Bernth
  * Robert Walters
@@ -11,8 +9,7 @@ package sequential;
 public class Seqnbody {
 
 	public static int workers, n, size, numSteps,dt;
-	public static Point[] p, v, f;
-	public static double[] m = new double[n];
+	public static Planet[] planets;
 	public static final double g = Math.pow(6.67,-11);
 
 	/*---------------------------------------------------
@@ -38,22 +35,15 @@ public class Seqnbody {
 			numSteps = Integer.valueOf(args[3]);
 			dt = 1;
 		}
-		
-		p = new Point[n];
-		v = new Point[n];
-		f = new Point[n];
-		m = new double[n];
 
-		GUI gui = new GUI("NBody Problem", p, size);
+		planets = initPlanets(n);
+		GUI gui = new GUI("NBody Problem", planets, size);
 		gui.setVisible(true);
-
-		initPVFM();
-		gui.update(p);
 
 		for (int time = 0; time < numSteps*dt; time++) {
 			calculateForces();
 			moveBodies();
-			gui.update(p);
+			gui.update();
 			// Sleep if you want to see the current parameters more slowly
 			try {Thread.sleep(100);}catch(InterruptedException e){}
 		}
@@ -69,20 +59,22 @@ public class Seqnbody {
 	 * Change the initial conditions for different
 	 * results.
 	 *---------------------------------------------------*/
-	public static void initPVFM() {
+	public static Planet[] initPlanets(int n) {
+		Planet[] out = new Planet[n];
 		int vxneg, vyneg, fxneg, fyneg;
 		for (int i = 0; i < n; i++) {
 			vxneg = (int)(Math.random()*2);
 			vyneg = (int)(Math.random()*2);
 			fxneg = (int)(Math.random()*2);
 			fyneg = (int)(Math.random()*2);
-			p[i] = new Point((Math.random()*1200),(Math.random()*600));
-			v[i] = new Point(vxneg == 1 ? Math.random()*-10 : Math.random()*10,
-							 vyneg == 1 ? Math.random()*-10 : Math.random()*10);
-			f[i] = new Point(fxneg == 1 ? Math.random()*-10 : Math.random()*10,
-							 fyneg == 1 ? Math.random()*-10 : Math.random()*10);
-			m[i] = Math.random()*100000;
+			out[i] = new Planet(new Point((Math.random()*1200),(Math.random()*600)),
+								new Point(vxneg == 1 ? Math.random()*-10 : Math.random()*10,
+										  vyneg == 1 ? Math.random()*-10 : Math.random()*10),
+								new Point(fxneg == 1 ? Math.random()*-10 : Math.random()*10,
+										  fyneg == 1 ? Math.random()*-10 : Math.random()*10),
+								Math.random()*100000);
 		}
+		return out;
 	}
 
 	/*---------------------------------------------------
@@ -95,14 +87,14 @@ public class Seqnbody {
 		Point direction;
 		for (int i = 1; i < n-1; i++) {
 			for (int j = i+1; j < n; j++) {
-				distance = Math.sqrt(Math.pow(p[i].getX()-p[j].getX(),2) +
-									 Math.pow(p[i].getY()-p[j].getY(),2));
-				magnitude = (g*m[i]*m[j]) / Math.pow(distance,2);
-				direction = new Point(p[j].getX()-p[i].getX(), p[j].getY()-p[i].getY());
-				f[i].setX(f[i].getX()+magnitude*direction.getX()/distance);
-				f[j].setX(f[j].getX()-magnitude*direction.getX()/distance);
-				f[i].setY(f[i].getY()+magnitude*direction.getY()/distance);
-				f[j].setY(f[j].getY()-magnitude*direction.getY()/distance);
+				distance = Math.sqrt(Math.pow(planets[i].p.getX()-planets[j].p.getX(),2) +
+									 Math.pow(planets[i].p.getY()-planets[j].p.getY(),2));
+				magnitude = (g*planets[i].m*planets[j].m) / Math.pow(distance,2);
+				direction = new Point(planets[j].p.getX()-planets[i].p.getX(), planets[i].p.getY()-planets[i].p.getY());
+				planets[i].f.setX(planets[i].f.getX()+magnitude*direction.getX()/distance);
+				planets[j].f.setX(planets[j].f.getX()-magnitude*direction.getX()/distance);
+				planets[i].f.setY(planets[i].f.getY()+magnitude*direction.getY()/distance);
+				planets[j].f.setY(planets[j].f.getY()-magnitude*direction.getY()/distance);
 			}
 		}
 	}
@@ -116,14 +108,14 @@ public class Seqnbody {
 		Point deltav, // dv = f/m * DT
 			  deltap; // dp = (v + dv/2) * DT
 		for (int i = 1; i < n; i++) {
-			deltav = new Point(f[i].getX()/m[i]*dt,f[i].getY()/m[i]*dt);
-			deltap = new Point((v[i].getX()+deltav.getX()/2)*dt,
-							   (v[i].getY()+deltav.getY()/2)*dt);
-			v[i].setX(v[i].getX()+deltav.getX());
-			v[i].setY(v[i].getY()+deltav.getY());
-			p[i].setX(p[i].getX()+deltap.getX());
-			p[i].setY(p[i].getY()+deltap.getY());
-			f[i].move(0.0,0.0); // reset force vector
+			deltav = new Point(planets[i].f.getX()/planets[i].m*dt,planets[i].f.getY()/planets[i].m*dt);
+			deltap = new Point((planets[i].v.getX()+deltav.getX()/2)*dt,
+							   (planets[i].v.getY()+deltav.getY()/2)*dt);
+			planets[i].v.setX(planets[i].v.getX()+deltav.getX());
+			planets[i].v.setY(planets[i].v.getY()+deltav.getY());
+			planets[i].p.setX(planets[i].p.getX()+deltap.getX());
+			planets[i].p.setY(planets[i].p.getY()+deltap.getY());
+			planets[i].f.move(0.0,0.0); // reset force vector
 		}
 	}
 
